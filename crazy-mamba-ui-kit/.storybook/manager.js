8a804f2f13f2ece9a500597437b9a9b7ccc39e3f
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-27 20:23:20 -0400

    add notes

---
+++ crazy-mamba-ui-kit/.storybook/manager.js
@@ -0,0 +1,2 @@
+// The manager.js is to control the behaviour of Storybook’s UI. You can set UI options and configure Storybook’s theme. It is optional and no need to add if your project doesn’t have the requirement for customised Storybook UI/theme.
+
